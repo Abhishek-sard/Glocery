c4ded159aa4a46b48278fcc12944c15a8f6a4e21
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-20 11:02:30 +0545

    push

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./Context/AppContext.jsx";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>
-);
+  <StrictMode>
+    <App/>
+  </StrictMode>
+)
+
