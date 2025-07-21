9a9480ebb99f56d6bc853352ab2ff69bb3a185cd
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-21 09:12:30 +0545

    push

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Body from './Components/Body';
-import BodyPart2 from './Components/BodyPart2';
-import Footer from './Components/Footer';
-const App = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Body/>
-      <BodyPart2/>
-      <Footer/>
-    </div>
-  )
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+//components for each routes
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+//components for Home pages
+import Body from "./Components/Body";
+import BodyPart2 from "./Components/BodyPart2";
+
+
+
+//components for Custom pages
+
+
+
+
+//components for About pages
+
+
+
+//components for New Arrivals pages
+
+
+
+
+
+//components for For Sale pages
+
+
+
+
+
+//components for Services pages
+
+
+
+
+function Home () {
+  return(
+    <>
+     <Body/>
+    <BodyPart2/>
+    </>
+  );
 }
 
-export default App
+
+function App() {
+  return(
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" elements={<Home/>}/>
+      </Routes>
+    </Router>
+  );
+}
 
 
-
+export default App;
